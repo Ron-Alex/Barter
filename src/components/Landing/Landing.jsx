7cfe7693125a355e910e158ac5851c8b88378449
Ron Alex <ronmathewalx@gmail.com>
2025-03-05 02:32:23 -0600

    Quick add

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,6 +1,6 @@
 const Landing = () => {
     return(
-        <h1 className="text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl">Welcome to Barter</h1>
+        <h1 className="text-5xl font-semibold tracking-tight text-balance text-white sm:text-7xl">Welcome to Barter</h1>
     )
 }
 
